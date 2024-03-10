7eb4aef36fc151de08878a6eab5317579bd1f4fe
Marcos Ibanez <mgi1982@gmail.com>
2024-03-10 10:29:59 -0700

    Added script to port knock using curl, nc or wget. In that order

---
+++ knockknock.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+PORTS="118 218 318"
+HOST=$1
+
+if [ -z "$1" ] ; then
+	echo "Where do you want me to knock?"
+    exit 1
+fi
+
+function knock() {
+    CMD=$(which curl)
+    if [ ! -z "$CMD" ] ; then
+        CMD="$CMD --connect-timeout 0.1 $HOST:$PORT"
+    else 
+        CMD=$(which nc)
+        if [ ! -z "$CMD" ] ; then
+            CMD="$CMD -G 1 $HOST $PORT"
+        else 
+            CMD=$(which wget)
+            if [ ! -z "$CMD" ] ; then
+                CMD="$CMD -t 1 --timeout=0.1 $HOST:$PORT"
+            fi 
+        fi 
+    fi 
+
+    if [ -z "$CMD" ] ; then
+        echo "No command available to knock. Exiting"
+        exit 1
+    else
+        $CMD
+    fi
+}
+
+echo "Knock Knock $HOST"
+for PORT in $PORTS
+do
+    knock
+done
