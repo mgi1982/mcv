9e7130f084af6db2612275c0d8b325e8287a12e0
Marcos Ibanez <mgi1982@gmail.com>
2025-01-15 16:02:02 -0800

    Added execute perms for scripts

--- rec-pull.sh
+++ rec-pull.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "Updating GIT repos"
-ODIR=`pwd`
-for i in `find $ODIR -name '.git' -type d | sed s@/.git@@g`
+ODIR=$(pwd)
+for i in $(find $ODIR -name '.git' -type d | sed s@/.git@@g)
 do
     cd $i
     echo "Updating $i"
