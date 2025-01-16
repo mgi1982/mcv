6b2aebe07fd0425fe7822fee7d3e5a8a56d697f7
Marcos Ibanez <mgi1982@gmail.com>
2025-01-15 16:03:23 -0800

    Merge pull request #8 from mgi1982/dev
    Mac and Manjaro packages updates

---
+++ bin/rec-pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Updating GIT repos"
+ODIR=$(pwd)
+for i in $(find $ODIR -name '.git' -type d | sed s@/.git@@g)
+do
+    cd $i
+    echo "Updating $i"
+    git pull
+done
+cd $ODIR
