8db6ae52e37045cd8a02d86735c8a805b88db26d
Marcos Ibanez <mgi1982@gmail.com>
2025-02-24 08:59:39 -0800

    Installing node after nvm

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -86,6 +86,7 @@ install_node() {
     fi
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+    nvm install node
 }
 
 install_binaries() {
